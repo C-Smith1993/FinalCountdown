ed41b9e29dc2384ce85104ef1c73568dfaf243e0
C-Smith1993 <chrissmithreds93@hotmail.com>
2022-08-25 20:01:29 +0100

    Displays hard coded events

--- Playground/View/EventListVC.swift
+++ Playground/View/EventListVC.swift
@@ -22,20 +22,22 @@ class EventListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
         
-        //Create some temp events
-        let event1 = Event(title: "Holiday", days: "1090")
-        let event2 = Event(title: "Brad's birthday", days: "209")
+        let event1 = Event(title: "Holiday", days: "234")
+        let event2 = Event(title: "Birthday", days: "189")
         
         eventsArray.append(event1)
         eventsArray.append(event2)
         
+        print(eventsArray)
+        
         tableView.reloadData()
     }
+
     
     //tableView Functions
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -43,11 +45,10 @@ class EventListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         let eventCell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
         
         //title is tag 1
-        //days is tag 2
-        
         let titleLabel = eventCell.viewWithTag(1) as! UILabel
         titleLabel.text = eventsArray[indexPath.row].title
         
+        //days is tag 2
         let daysLabel = eventCell.viewWithTag(2) as! UILabel
         daysLabel.text = eventsArray[indexPath.row].days
         
@@ -55,6 +56,6 @@ class EventListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return eventsArray.count
     }
 }
